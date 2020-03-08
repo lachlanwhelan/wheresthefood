0d71126265dedb0cd87bfd3f61ec4a0ea9564ca7
Lachlan Whelan <lachlan.whelan1@gmail.com>
2020-03-08 17:30:29 +1100

    Updates

---
+++ precache-manifest.9acb456b8caa17f81a00085ec20cb260.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4512cfcb045e91e678e9797b5f58db22",
+    "url": "/wheresthefood/index.html"
+  },
+  {
+    "revision": "fbf95be7e6851c45e197",
+    "url": "/wheresthefood/static/css/main.82133062.chunk.css"
+  },
+  {
+    "revision": "e112a35bc9875d4d1669",
+    "url": "/wheresthefood/static/js/2.e3a07384.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wheresthefood/static/js/2.e3a07384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf95be7e6851c45e197",
+    "url": "/wheresthefood/static/js/main.c75d1bb3.chunk.js"
+  },
+  {
+    "revision": "dd7646b27cddb3691502",
+    "url": "/wheresthefood/static/js/runtime-main.15dc41d8.js"
+  },
+  {
+    "revision": "e0b45e7f72be171303d99e3ec40f89a4",
+    "url": "/wheresthefood/static/media/defaultImage.e0b45e7f.jpg"
+  },
+  {
+    "revision": "1151080c6bb41e3e6337d71e8f7b03dc",
+    "url": "/wheresthefood/static/media/loading.1151080c.gif"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/wheresthefood/static/media/star.45dd5470.png"
+  }
+]);
